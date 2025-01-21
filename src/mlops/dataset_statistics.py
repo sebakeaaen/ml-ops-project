a0ea60174dcae3e5dae09104f85ac8170bab5cb7
sebakeaaen <10397702+sebakeaaen@users.noreply.github.com>
2025-01-21 03:19:16 +0100

    cml test

--- src/mlops/dataset_statistics.py
+++ src/mlops/dataset_statistics.py
@@ -1,3 +1,4 @@
+import os
 import typer
 from mlops.utils import show_image_and_target
 from matplotlib import pyplot as plt
@@ -7,14 +8,19 @@ from mlops.data import load_data, PistachioDataset
 def dataset_statistics() -> None:
     """Compute dataset statistics."""
     train_dataset = PistachioDataset("data/raw")
-    print("Pistachio dataset")
-    print(f"Number of images: {len(train_dataset)}")
-    print(f"Image shape: {train_dataset[0][0].shape}")
+
+    if not os.path.exists("temp"):
+        os.makedirs("temp", exist_ok=True)
+
+    with open("temp/dataset_statistics.txt", "w") as f:
+        f.write("Pistachio dataset\n")
+        f.write(f"Number of images: {len(train_dataset)}\n")
+        f.write(f"Image shape: {train_dataset[0][0].shape}\n")
 
     train_dataset, test_dataset = load_data()
     images, targets = next(iter(train_dataset))
     show_image_and_target(images[:4], targets[:4], show=False)
-    plt.savefig("reports/figures/pistachio_images.png")
+    plt.savefig("temp/pistachio_images.png")
     plt.close()
 
 
