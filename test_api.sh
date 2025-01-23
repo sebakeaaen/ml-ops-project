f0c6e87c382660611785147946ff89a4f9372b5d
sebakeaaen <10397702+sebakeaaen@users.noreply.github.com>
2025-01-23 18:56:14 +0100

    test full CICD

--- test_api.sh
+++ test_api.sh
@@ -1,5 +1,5 @@
 curl -X 'POST' \
-  'http://0.0.0.0:8080/classify/' \
+  'https://pistachio-api-266814087920.europe-west3.run.app/classify/' \
   -H 'accept: application/json' \
   -H 'Content-Type: multipart/form-data' \
   -F 'data=@data/raw/Pistachio_Image_Dataset/Pistachio_Image_Dataset/Kirmizi_Pistachio/kirmizi (2).jpg'
